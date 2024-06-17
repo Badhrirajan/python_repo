48f603841c8d4875a0d7bc8e72a838775ea1b76e
Badhrirajan <badhrirajan2211@gmail.com>
2024-06-17 21:49:48 +0530

    Changed program

--- calculator.py
+++ calculator.py
@@ -1,23 +1,30 @@
 def add(x,y):
+    x = int(input("Enter the x value : "))
+    y = int(input("Enter the y value : "))
     print(x + y)
 
 def sub(x,y):
+    x = int(input("Enter the x value : "))
+    y = int(input("Enter the y value : "))
     print(x - y)
 
 def Mul(x,y):
+    x = int(input("Enter the x value : "))
+    y = int(input("Enter the y value : "))
     print(x * y)
 
 def Div(x,y):
+    x = int(input("Enter the x value : "))
+    y = int(input("Enter the y value : "))
     print(x / y)
 
 def square(x):
+    x = int(input("Enter the x value : "))
     print(x ** 2)
 
 def cube(y):
+    y = int(input("Enter the y value : "))
     print(y ** 3)
-
-x = int(input("Enter the x value : "))
-y = int(input("Enter the y value : "))
 
 print("""1.Add, 2.Sub, 3.Mul, 4.Div,5.Square,6.Cube""")
 
