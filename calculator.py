479ca05961bd04818c7d53fb0b3b0e32fdf0b302
Badhrirajan <badhrirajan2211@gmail.com>
2024-06-17 21:12:50 +0530

    First draft code

--- calculator.py
+++ calculator.py
@@ -10,13 +10,10 @@ def Mul(x,y):
 def Div(x,y):
     print(x / y)
 
-def percent(x):
-    print(x / 100 * 100)
-
 x = int(input("Enter the x value : "))
 y = int(input("Enter the y value : "))
 
-print("""1.Add, 2.Sub, 3.Mul, 4.Div,5.Percent""")
+print("""1.Add, 2.Sub, 3.Mul, 4.Div""")
 
 while True:
     user_choice = int(input("Enter the choice : "))
@@ -28,8 +25,6 @@ while True:
             Mul(x,y)
     elif user_choice == 4:
             Div(x,y)
-    elif user_choice == 5:
-            percent(x)
     else:
         print("Invalid Option")
 
