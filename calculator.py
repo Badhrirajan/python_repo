188d7c5f90919a5c0347aaab46f49115ee918fec
Badhrirajan <badhrirajan2211@gmail.com>
2024-06-17 16:30:49 +0530

    Added percentage function

--- calculator.py
+++ calculator.py
@@ -10,10 +10,13 @@ def Mul(x,y):
 def Div(x,y):
     print(x / y)
 
+def percent(x):
+    print(x / 100 * 100)
+
 x = int(input("Enter the x value : "))
 y = int(input("Enter the y value : "))
 
-print("""1.Add, 2.Sub, 3.Mul, 4.Div""")
+print("""1.Add, 2.Sub, 3.Mul, 4.Div,5.Percent""")
 
 while True:
     user_choice = int(input("Enter the choice : "))
@@ -25,6 +28,8 @@ while True:
             Mul(x,y)
     elif user_choice == 4:
             Div(x,y)
+    elif user_choice == 5:
+            percent(x)
     else:
         print("Invalid Option")
 
