6ab9a49fd9d35e6b02957364c1b1d6e9cbb22d86
Badhrirajan <badhrirajan2211@gmail.com>
2024-06-17 16:22:31 +0530

    First draft

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def add(x,y):
+    print(x + y)
+
+def sub(x,y):
+    print(x - y)
+
+def Mul(x,y):
+    print(x * y)
+
+def Div(x,y):
+    print(x / y)
+
+x = int(input("Enter the x value : "))
+y = int(input("Enter the y value : "))
+
+print("""1.Add, 2.Sub, 3.Mul, 4.Div""")
+
+while True:
+    user_choice = int(input("Enter the choice : "))
+    if user_choice == 1:
+            add(x,y)
+    elif user_choice == 2:
+            sub(x,y)
+    elif user_choice == 3:
+            Mul(x,y)
+    elif user_choice == 4:
+            Div(x,y)
+    else:
+        print("Invalid Option")
+
